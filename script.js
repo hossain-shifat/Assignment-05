07d538afd1c14f4397434ffa26ef2e4b7192857d
Hossain-Shifat <161940464+hossain-shifat@users.noreply.github.com>
2025-08-28 18:17:18 +0600

    Add call history functionality

--- script.js
+++ script.js
@@ -22,9 +22,9 @@ copyBtn.forEach(button =>{
         const card = button.closest(".card");
         const copyInnerText = card.querySelector(".number").innerText
 
-        // cpoy inner text
+        // cpoy inner text and add 1 in copy count
         navigator.clipboard.writeText(copyInnerText).then(function() {
-            alert(`Copied number: ${copyInnerText}`)
+            alert(`Number copid: ${copyInnerText}`)
             const copyCountNum = parseInt(copyCount.innerText)
             copyCount.innerText = copyCountNum + 1
         })
@@ -35,6 +35,10 @@ copyBtn.forEach(button =>{
 
 const callBtn = document.querySelectorAll(".call-btn")
 const coinCount = document.getElementById("coin-count")
+const callingDetailContainerMain = document.getElementById("calling-detail-container-main")
+
+
+
 
 callBtn.forEach(button =>{
     button.addEventListener("click",function(){
@@ -57,6 +61,53 @@ callBtn.forEach(button =>{
         const coinCountNum = parseInt(coinCount.innerText)
         coinCount.innerText = coinCountNum - 20
 
-        // here call histiry
+
+        const callingDetailContainer = document.createElement("div")
+        callingDetailContainer.className = "flex justify-between items-center my-2"
+
+        //calling detail parent
+        const callingDetailDiv = document.createElement("div")
+        callingDetailDiv.id = "calling-detail"
+
+        //adding the call title in call history section
+        const callTitle = document.createElement("p")
+        callTitle.className = "text-[#5C5C5C] text-[1.1rem]"
+        callTitle.innerText = subtitle
+        callingDetailDiv.appendChild(callTitle)
+
+        //adding the number in call history section
+        const callNum = document.createElement("h2")
+        callNum.className = "text-xl md:text-[1.25rem] font-bold py-2 number"
+        callNum.innerText = serviceNum
+        callingDetailDiv.appendChild(callNum)
+
+        // time div
+        const callingTimeDiv = document.createElement("div")
+        callingDetailDiv.id = "calling-time"
+
+
+        //  get current time
+        const currentTime = new Date()
+        const currentLocaleTime = currentTime.toLocaleTimeString()
+
+
+        //adding the time in call history section
+        const time = document.createElement("h1")
+        time.className = "text-[#5C5C5C] text-[1.1rem] font-[Roboto]"
+        time.innerText = currentLocaleTime
+        callingTimeDiv.appendChild(time)
+
+
+
+        callingDetailContainer.appendChild(callingDetailDiv)
+        callingDetailContainer.appendChild(callingTimeDiv)
+
+        // adding the calling-detail to the parent div (calling-detail-container)
+        callingDetailContainerMain.appendChild(callingDetailContainer)
+
+
     })
 })
+
+
+
