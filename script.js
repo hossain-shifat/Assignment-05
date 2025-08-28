f720419f07d94b5f5f94444d8ec9b41064ae0517
Hossain-Shifat <161940464+hossain-shifat@users.noreply.github.com>
2025-08-28 12:45:10 +0600

    Add call functionality

--- script.js
+++ script.js
@@ -16,5 +16,32 @@ copyBtn.forEach(button =>{
     })
 })
 
+//  call functionality
 
+const callBtn = document.querySelectorAll(".call-btn")
+const coinCount = document.getElementById("coin-count")
 
+callBtn.forEach(button =>{
+    button.addEventListener("click",function(){
+        const card = button.closest(".card");
+        const subtitle = card.querySelector(".subtitle").innerText
+        const serviceNum = card.querySelector(".number").innerText
+
+        // checking the balence
+        if (parseInt(coinCount.innerText)===0) {
+            alert("Insufficient Balance")
+            return
+        }
+
+        // if have sufficient balence
+
+        alert(`📞Calling ${subtitle} ${serviceNum}...`);
+
+        // decreasing 20 conin for each call
+
+        const coinCountNum = parseInt(coinCount.innerText)
+        coinCount.innerText = coinCountNum - 20
+
+        // here call histiry
+    })
+})
